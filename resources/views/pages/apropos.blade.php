399fe85ae3de738421f7837863d86651388dbfdf
goetechs <goldjohn120@gmail.com>
2023-07-29 19:15:44 +0000

    add de proper title

--- resources/views/pages/apropos.blade.php
+++ resources/views/pages/apropos.blade.php
@@ -1,26 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hello city | a propos</title>
-</head>
-<body>
+@extends('base')
+
+@section('title', 'Hello city | a propos ')
 
 
 
-<section>
-    
+@section('content')
 
     <p>Creer avec &hearts; par Jean claude SASSOU</p>
 
     <a href="/">Revenir a la page d'accueil</a>
     <h1> Dans a propos</h1>
-</section>
 
-<footer>
-    <p>&copy; Copyright in 2023  </p>
-</footer>
-       
-</body>
-</html>
+@endsection
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,23 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hello city</title>
-</head>
-<body>
+@extends('base')
 
 
 
-<section>
+@section('content')
+
     <h1> Je suis content</h1>
 
     <p> Il est actuellement : {{ date('H:i ')}}</p>
-</section>
 
-<footer>
-    <p>&copy; Copyright 2023 &middot; <a href="/apropos">A propos</a></p>
-</footer>
-    
-</body>
-</html>
+@endsection
